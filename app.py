a14bbdddf6d9516c887a1a11db795903fe486948
nagi <99517580+nagi-hobbies@users.noreply.github.com>
2023-04-17 23:43:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,58 @@
 import streamlit as st
 import pandas as pd
+import re
 
 
-def can_convert_to_float(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
+class MyValueError(Exception):
+    pass
 
 
 def parse_grade(grade_text):
     grade_list = []
+    main_category = ''
+    sub_category = ''
     lines = grade_text.split('\n')
     for line in lines:
-        cols = line.split()
-        if len(cols) >= 5:
-            name = cols[0]
-            grade = str(cols[-3])
-            if can_convert_to_float(cols[-4]):
-                crdt = float(cols[-4])
-            else:
-                grade = str(cols[-4]) + grade
-                crdt = float(cols[-5])
-            grade_list.append([name, crdt, grade])
-    grade_data = pd.DataFrame(grade_list, columns=['教科名', '単位数', '成績'])
+        cols = re.split('[ \t]+', line)
+        if len(cols) == 1:
+            main_category = cols[0]
+        elif len(cols) == 2:
+            sub_category = cols[1]
+        elif len(cols) == 8:
+            cols[-2] = int(cols[-2])
+            cols[-4] = float(cols[-4])
+            grade_list.append([main_category, sub_category, *cols[1:]])
+        else:
+            raise MyValueError()
+    grade_data = pd.DataFrame(
+        grade_list,
+        columns=['科目分類(大)',
+                 '科目分類(小)',
+                 '教科名',
+                 '教授名',
+                 '選択or必修or?',
+                 '単位数',
+                 '成績',
+                 '取得年度',
+                 '前期or後期'])
     return grade_data
+
+# def parse_grade(grade_text):
+#     grade_list = []
+#     lines = grade_text.split('\n')
+#     for line in lines:
+#         cols = line.split()
+#         if len(cols) >= 5:
+#             name = cols[0]
+#             grade = str(cols[-3])
+#             if can_convert_to_float(cols[-4]):
+#                 crdt = float(cols[-4])
+#             else:
+#                 grade = str(cols[-4]) + grade
+#                 crdt = float(cols[-5])
+#             grade_list.append([name, crdt, grade])
+#     grade_data = pd.DataFrame(grade_list, columns=['教科名', '単位数', '成績'])
+#     return grade_data
 
 
 def get_gpa(ug_arr):
@@ -63,18 +90,22 @@ score_text = st.text_area("↓ここにペースト↓", placeholder="""
              電磁気学Ⅰ	 	  教授　花子	  必修	  2	 	  Ｂ	2021	  後期
 ... (省略) ...
 """, height=300)
+
 if st.button('計算') and score_text:
+
     try:
         grade_data = parse_grade(score_text)
-        gpa, total_credits = get_gpa(grade_data[['単位数', '成績']].values)
-        st.write('あなたのGPAは {:.2f} (総取得単位数{})'.format(gpa, total_credits))
-        st.dataframe(grade_data.round(2))
-        st.download_button(
-            label='Download .csv',
-            data=grade_data.to_csv(index=False).encode('utf-8'),
-            file_name='grade_data.csv',
-            mime='text/csv'
-        )
-        st.write('Excelで文字化けする場合は文字コードをUTF-8に変更してください')
     except:
         st.write('成績の形式がおかしいようです')
+        st.stop()
+    gpa, total_credits = get_gpa(grade_data[['単位数', '成績']].values)
+    st.write('GPA:{:.2f} , 総取得単位数:{}'.format(gpa, total_credits))
+    st.write('見出しをクリックすると並べ替えができます')
+    st.dataframe(grade_data.round(2))
+    st.download_button(
+        label='Download .csv',
+        data=grade_data.to_csv(index=False).encode('utf-8'),
+        file_name='grade_data.csv',
+        mime='text/csv'
+    )
+    st.write('Excelで文字化けする場合は文字コードをUTF-8に変更してください')
