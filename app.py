c20c7df4d72264ada95c3a8248dc576c79173088
nagi <99517580+nagi-hobbies@users.noreply.github.com>
2024-02-11 23:09:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sys
 
+import pandas as pd
+
 import streamlit as st
 
 import gpa
@@ -39,29 +41,39 @@ if st.button("計算") and text:
         st.write("成績の形式がおかしいようです")
         st.write("エラー場所：", e.line)
         st.stop()
-    # file_name = "grade_pc.txt"
-    # with open(file_name, "r", encoding="utf-8") as f:
-    #     txt_grade = f.read()
-    # df_grade =new.text2df(txt_grade)
-    total_credits = df_grade["単位"].sum()
-    gpa = sum(df_grade["GPA換算成績"] * df_grade["単位"]) / total_credits
+        
 
-    category_credit = df_grade.groupby("科目分類(大)")["単位"].sum()
-    sub_category_credit = df_grade.groupby(["科目分類(大)", "科目分類(小)"])["単位"].sum()
+    st.write("---")
 
     st.header("GPA計算結果")
+    total_credits = df_grade[df_grade["GPA換算成績"] != 0.0]["単位"].sum()
+    gpa = sum(df_grade["GPA換算成績"] * df_grade["単位"]) / total_credits
     st.write("GPA:{:.2f} , 総取得単位数:{}".format(gpa, total_credits))
-    st.write("履修登録後かつ成績発表前で，単位数や成績が空欄のものは，単位数0，成績0として(存在しないものとして)計算しています。")
 
-    st.header("科目分類(大)ごとの単位数")
-    st.table(category_credit)
-    st.header("科目分類(小)ごとの単位数")
-    st.table(sub_category_credit)
+    st.subheader("GPAの計算方法")
+    """
+    - 以下のように成績をGPAに換算して，単位数をかけて合計し，総取得単位数により平均をとっています。
+
+
+            
+        | 成績 | GPA |
+        |------|---------|
+        | AA or 100~90 | 4.0     |
+        | A  or  89~80 | 3.0     |
+        | B  or  79~70 | 2.0     |
+        | C  or  69~60 | 1.0     |
+        | D  or  59~   | 0.0     |
+    
+
+
+    - 履修登録後かつ成績発表前で，単位数や成績が空欄のものは，単位数0，成績0として(存在しないものとして)計算しています。
+    """
+    st.write("---")
 
     st.header("成績データ")
     st.write("見出しをクリックすると並べ替えができます")
     st.dataframe(df_grade)
-    
+
     st.download_button(
         label="Download .csv",
         data=df_grade.to_csv(index=False),
@@ -69,5 +81,26 @@ if st.button("計算") and text:
         mime="text/csv",
     )
     st.write("Excelで文字化けする場合は文字コードをUTF-8に変更してください")
+
+    st.write("---")
+
+    st.header("科目分類(大)ごとの単位数")
+    category_credit = (
+        df_grade[df_grade["GPA換算成績"] != 0.0].groupby("科目分類(大)")["単位"].sum()
+    )
+    st.table(category_credit)
+
+    st.write("---")
+
+    st.header("科目分類(小)ごとの単位数")
+    sub_category_credit = (
+        df_grade[df_grade["GPA換算成績"] != 0.0]
+        .groupby(["科目分類(大)", "科目分類(小)"])["単位"]
+        .sum()
+    )
+    st.table(sub_category_credit)
+
+    st.write("---")
+
 url = "https://github.com/nagi-hobbies/tohoku-gpa"
 st.write("ソースコードはこちら [Git Hub](%s)" % url)
