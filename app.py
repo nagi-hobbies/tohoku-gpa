1e7f34b25474d31ec804f787f9748380b68f13fc
nagi <99517580+nagi-hobbies@users.noreply.github.com>
2024-02-11 19:09:46 +0900

    update logic

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ import sys
 
 import streamlit as st
 
-from gpa import *
+import gpa
 
 st.title("Tohoku Univ GPA Calculator")
 st.write("学務情報システムからコピー&ペースト(マウスでドラッグして青くするやつ)")
-score_text = st.text_area(
+text = st.text_area(
     "↓ここにペースト↓",
     placeholder="""
+HOME
+教務
+履修・成績
+シラバス
+学生支援
+... (省略) ...
 全学教育科目基幹科目
      人間論
              言語表現の世界	 	  先生　太郎	  選択	  2	 	  ＡＡ	2021	  前期前半
@@ -16,25 +22,49 @@ score_text = st.text_area(
      理学部専門教育科目
              電磁気学Ⅰ	 	  教授　花子	  必修	  2	 	  Ｂ	2021	  後期
 ... (省略) ...
+成績照会
+ｵﾝﾗｲﾝﾏﾆｭｱﾙ
+履修カルテ
+
+PAGE TOP
+
 """,
     height=300,
 )
 
-if st.button("計算") and score_text:
+if st.button("計算") and text:
     try:
-        grade_data = parse_grade(score_text)
-    except MyValueError as e:
+        df_grade = gpa.text2df(text)
+    except gpa.MyValueError as e:
         st.write("成績の形式がおかしいようです")
-        # cols = re.split('[ \t]+', e.line)
         st.write("エラー場所：", e.line)
         st.stop()
-    gpa, total_credits = get_gpa(grade_data[["単位数", "成績"]].values)
+    # file_name = "grade_pc.txt"
+    # with open(file_name, "r", encoding="utf-8") as f:
+    #     txt_grade = f.read()
+    # df_grade =new.text2df(txt_grade)
+    total_credits = df_grade["単位"].sum()
+    gpa = sum(df_grade["GPA換算成績"] * df_grade["単位"]) / total_credits
+
+    category_credit = df_grade.groupby("科目分類(大)")["単位"].sum()
+    sub_category_credit = df_grade.groupby(["科目分類(大)", "科目分類(小)"])["単位"].sum()
+
+    st.header("GPA計算結果")
     st.write("GPA:{:.2f} , 総取得単位数:{}".format(gpa, total_credits))
+    st.write("履修登録後かつ成績発表前で，単位数や成績が空欄のものは，単位数0，成績0として(存在しないものとして)計算しています。")
+
+    st.header("科目分類(大)ごとの単位数")
+    st.table(category_credit)
+    st.header("科目分類(小)ごとの単位数")
+    st.table(sub_category_credit)
+
+    st.header("成績データ")
     st.write("見出しをクリックすると並べ替えができます")
-    st.dataframe(grade_data.round(2))
+    st.dataframe(df_grade)
+    
     st.download_button(
         label="Download .csv",
-        data=grade_data.to_csv(index=False).encode("utf-8"),
+        data=df_grade.to_csv(index=False),
         file_name="grade_data.csv",
         mime="text/csv",
     )
--- gpa.py
+++ gpa.py
@@ -1,7 +1,5 @@
 import re
-
 import pandas as pd
-
 
 class MyValueError(Exception):
     def __init__(self, line):
@@ -10,73 +8,82 @@ class MyValueError(Exception):
     pass
 
 
-def parse_grade(grade_text):
-    grade_list = []
-    main_category = ""
-    sub_category = ""
-    main_text = grade_text.split("年度	期間")[-1]
+def text2df(txt_grade):
+
+    main_text = txt_grade.split("年度	期間")[-1]
     main_text = main_text.split("修得単位状況")[0]
     lines = main_text.split("\n")
+
+    main_category = ""
+    sub_category = ""
+
+    grade_list = []
     for line in lines:
         cols = re.split("[ \t]+", line)
-        if len(cols) == 1:
+        if len(cols) == 1:  # [''] or ['main_category']
+            if cols[0] == "":
+                continue
             main_category = cols[0]
-        elif len(cols) == 2:
+        elif len(cols) == 2:  # ['', 'sub_category']
             sub_category = cols[1]
-        elif len(cols) == 8:
-            cols[-2] = int(cols[-2])
-            cols[-4] = float(cols[-4])
+        elif (
+            len(cols) == 8
+        ):  # ['', '教科名', '教授名', '選択or必修or?', '単位数', '成績', '取得年度', '前期or後期']
+            cols[-2] = int(cols[-2])  # 取得年度
+            cols[-4] = float(cols[-4])  # 単位数
+            cols.insert(-2, grade2gpa(cols[-3]))
             grade_list.append([main_category, sub_category, *cols[1:]])
-        elif len(cols) == 6:
-            pass
+        elif (
+            len(cols) == 6
+        ):  # ['', '教科名', '教授名', '選択or必修or?', '取得年度', '前期or後期']
+            cols[-2] = int(cols[-2])  # 取得年度
+            cols.insert(-2, "")  # 成績
+            cols.insert(-3, 0.)  # 単位数
+            cols.insert(-2, 0)  # GPA
+            grade_list.append([main_category, sub_category, *cols[1:]])
         else:
             raise MyValueError(line)
-    grade_data = pd.DataFrame(
+    df_grade = pd.DataFrame(
         grade_list,
         columns=[
             "科目分類(大)",
             "科目分類(小)",
-            "教科名",
-            "教授名",
-            "選択or必修or?",
-            "単位数",
-            "成績",
-            "取得年度",
-            "前期or後期",
+            "科目名",
+            "担当教員",
+            "選択／必修",
+            "単位",
+            "評価",
+            "GPA換算成績",
+            "年度",
+            "期間",
         ],
     )
-    return grade_data
+    return df_grade
 
-
-def get_gpa(ug_arr):
-    """
-    ug_arr: 2次元配列，単位数と成績の配列
-    """
-    gpa = 0
-    total_credits = 0
-    for ug in ug_arr:
-        crdt = ug[0]
-        grade = ug[1]
-        if grade == "ＡＡ":
-            gpa += 4 * crdt
-        elif grade == "Ａ":
-            gpa += 3 * crdt
-        elif grade == "Ｂ":
-            gpa += 2 * crdt
-        elif grade == "Ｃ":
-            gpa += 1 * crdt
-        else:
-            continue
-        total_credits += crdt
-    gpa = gpa / total_credits
-    return gpa, total_credits
+def grade2gpa(grade):
+    if grade == "ＡＡ":
+        return 4
+    elif grade == "Ａ":
+        return 3
+    elif grade == "Ｂ":
+        return 2
+    elif grade == "Ｃ":
+        return 1
+    else:
+        return 0
 
 
 if __name__ == "__main__":
-    # file_name = "grade.txt"
     file_name = "grade_phone.txt"
+    file_name = "grade_pc.txt"
     with open(file_name, "r", encoding="utf-8") as f:
-        text = f.read()
-    gpa_data = parse_grade(text)
-    gpa = get_gpa(gpa_data[["単位数", "成績"]].values)
-    print(gpa)
+        txt_grade = f.read()
+    text2df(txt_grade)
+
+    df_grade = text2df(txt_grade)
+
+    sum_credits = df_grade["単位"].sum()
+    gpa = (df_grade["GPA換算成績"] * df_grade["単位"]).sum() / sum_credits
+    print("GPA: {:.2f}".format(gpa))
+    print("総取得単位数: {}".format(sum_credits))
+
